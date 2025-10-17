65528ac041918e58a4ab894a6473d4295d3b1863
Gustavo Nogueira <144605556+Gustavo-Nog@users.noreply.github.com>
2025-10-17 02:32:15 +0000

    feat(models): exporta todas as rotas

---
+++ src/models/index.ts
@@ -0,0 +1,5 @@
+// Exporta todos os models
+export { Perfil } from './Perfil';
+export { PlanoAula } from './PlanoAula';
+export { TemplatePlano } from './TemplatePlano';
+export { HistoricoGeracao } from './HistoricoGeracao';
